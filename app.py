70dc1c045791a27b9d39c7f9cb14b6635ad70871
sven110sugar <wdzjg4d74w@privaterelay.appleid.com>
2026-06-15 19:29:39 +0800

    Initial commit - app files

---
+++ app.py
@@ -0,0 +1,12 @@
+from flask import Flask
+import os
+
+app = Flask(__name__)
+
+@app.route('/')
+def hello():
+    version = os.environ.get('APP_VERSION', 'v0.1.0')
+    return f'Hello from GitOps! Version: {version}'
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
