e3fa84b267efb2d4f2671803770aa2108f121a04
sven110sugar <wdzjg4d74w@privaterelay.appleid.com>
2026-06-16 12:14:07 +0800

    Update app.py

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello():
-    version = os.environ.get('APP_VERSION', 'v0.1.2')
+    version = os.environ.get('APP_VERSION', 'v0.1.3')
     return f'Hello from GitOps! Version: {version}'
 
 if __name__ == '__main__':
